ba1829550771e259377c61489e222df842bfd67e
Víctor Valencia <vvalencia@mercury.cash>
2022-10-28 14:40:34 -0500

    feat(github.service): request to backend API

---
+++ github-repo-front/src/services/github.service.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export class GithubService {
+    constructor() {
+        this.request = axios.create({
+            baseURL: 'localhost:3000/api'
+        });
+    }
+    async getCurrentUser() {
+        let ok = true;
+        let data = null;
+        try {
+            const response = await this.request.get('/user');
+            data = response.data;
+        } catch (error) {
+            console.log(error);
+            ok = false
+        }
+        return { ok, data };
+    }
+    async getCurrentRepo() {
+        let ok = true;
+        let data = null;
+        try {
+            const response = await this.request.get('/repo');
+            data = response.data;
+        } catch (error) {
+            console.log(error);
+            ok = false
+        }
+        return { ok, data };
+    }
+    async getCurrentRepositoryCommits() {
+        let ok = true;
+        let data = [];
+        try {
+            const response = await this.request.get('/repo/commits');
+            data = response.data;
+        } catch (error) {
+            console.log(error);
+            ok = false
+        }
+        return { ok, data };
+    }
+}

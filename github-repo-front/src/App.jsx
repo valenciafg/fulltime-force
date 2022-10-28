14ffec32b7fd2bf3dac437d56dd9512a44231ad3
Víctor Valencia <vvalencia@mercury.cash>
2022-10-28 08:06:32 -0500

    initial commit

---
+++ github-repo-front/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className='bg-black h-screen'>
+      <h1 className="text-3xl text-indigo-500 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
